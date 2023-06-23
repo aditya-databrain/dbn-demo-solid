1cf0aed77e2a5f605671ae12e164d56c11a0e00f
Aditya Yaduvanshi <aditya@usedatabrain.com>
2023-06-23 16:05:41 +0530

    fix dashboard id prop as kebab case

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App: Component = () => {
   const urlParams = new URLSearchParams(location.search);
   const token = urlParams.get("token");
   const dashboardId = urlParams.get("dashboardId");
-  return <dbn-dashboard token={token} dashboardId={dashboardId} />;
+  return <dbn-dashboard token={token} dashboard-id={dashboardId} />;
 };
 
 export default App;
